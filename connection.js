c36fa34c2bd76aa239c6efe11b89be6c33ee95f1
Danverde <gre12014@byui.edu.com>
2017-02-02 09:29:13 -0700

    error fixing

--- connection.js
+++ connection.js
@@ -6,14 +6,6 @@ var fs = require("fs");
 var options = require('./options.js');
 
 
-var sandboxML = "ML_a4VxkqLdvZQeeEJ";
-var MLRP = 'MLRP_25iWJEv63JRvDCZ';
-var baseURL = "https://az1.qualtrics.com/API/v3/mailinglists";
-
-
-
-
-
 function getMailingList(options) {
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
@@ -86,19 +78,19 @@ function updateStudent(options) {
 function main(action) {
     switch (action) {
         case "1":
-            getMailingList(options);
+            getMailingList(options[0]);
             break;
         case "2":
-            getStudents(options2);
+            getStudents(options[1]);
             break;
         case "3":
-            createStudent(options3);
+            createStudent(options[2]);
             break;
         case "4":
-            deleteStudent(options4);
+            deleteStudent(options[3]);
             break;
         case "5":
-            updateStudent(options5);
+            updateStudent(options[5]);
             break;
         default:
             console.log("Please enter a valid command:\n 1 Get Mailing List\n 2 Get Students\n 3 Create Student\n 4 Remove Student\n 5 Update Student");
