a9d440eecda07529e063746d79f677426aff156d
Danverde <gre12014@byui.edu.com>
2017-02-06 10:39:29 -0700

    WIP working on binary search and generating the toDelete array

--- main.js
+++ main.js
@@ -8,24 +8,57 @@ const StudentSnatcher = require('./import.js'),
     ss = new StudentSnatcher();
 
 function sortList(a, b) {
-    if (a.criteria < b.criteria) return -1;
-    if (a.criteria > b.criteria) return 1;
+    if (a.externalDataReference < b.externalDataReference) return -1;
+    if (a.externalDataReference > b.externalDataReference) return 1;
     return 0;
+}
+
+function formatStudents(students) {
+    var fStudents = students.map(function (currVal, fStudents) {
+        var tStudent = {};
+        tStudent = {
+            id: "",
+            firstName: currVal.PreferredName,
+            email: currVal.Email,
+            externalDataReference: currVal.UniqueID,
+            embeddedData: {
+                sessionOrder: currVal.SessionOrder,
+                username: currVal.Username,
+                semester: currVal.Semester,
+                course: currVal.Course,
+                section: currVal.Section,
+                subprogram: currVal.Subprogram,
+                department: currVal.Department,
+                pathway: currVal.Pathway,
+                block: currVal.Block,
+                gender: currVal.Gender,
+                countryOnline: currVal.CountryOnline,
+                state: currVal.State,
+                classification: currVal.Classification,
+                historicalAge: currVal.HistoricalAge,
+                major: currVal.Major
+            }
+        };
+        return tStudent;
+    });
+    return fStudents;
 }
 
 function init() {
     // get students from the tsv file
     ss.getStudents(function (students) {
+        // format tsv student object for qualtrics
+        students = formatStudents(students);
         students.sort(sortList);
 
-        //console.log("FROM LIST\n");
-        //console.log(students);
+        //        console.log("FROM LIST\n");
+        //        console.log(students);
 
         // get students from qualtrics
         ss.pullStudents(options[1], function (qStudents) {
 
-            //console.log("FROM QUALTRICS\n");
-            //console.log(qStudents);
+            //            console.log("FROM QUALTRICS\n");
+            //            console.log(qStudents);
             qStudents.sort(sortList);
             processTheData(students, qStudents);
         });
@@ -33,80 +66,64 @@ function init() {
 }
 
 function deleteStudents(toTerminate) {
-    //get unique qualtrics ID and send to deleteStudent
+    // get unique qualtrics ID and send to ss.deleteStudent
+}
+
+function addStudent(toAdd) {
+    // loop through array & call ss.addStudent
+}
+
+function updateStudent() {
+    // get unique qualtrics ID & send to ss.updateStudent
 }
 
 function processTheData(students, qStudents) {
     var toAdd = [],
         toUpdate = [];
 
+
+    // array containing unique id of all qualtrics students
+    /*var qSIndex = qStudents.map(function (currVal) {
+    return currVal.externalDataReference;
+});*/
     students.forEach(function (student, studentI) {
-        var qStudentI = bs(qStudents, student, function (value, find) {
-            if (value.externalDataReference < find.UniqueID) return 1;
-            else if (value.externalDataReference > find.UniqueID) return -1;
+        var qStudentI = bs(qSIndex, student, function (value, find) {
+            if (value.externalDataReference < find.externalDataReference) return 1;
+            if (value.externalDataReference > find.externalDataReference) return -1;
             else return 0;
         });
+        console.log(qStudentI);
+
         //if exists in qualtrics AND students are not the same
         if (qStudentI > -1 && !Object.is(student, qStudents[qStudentI])) {
             toUpdate.push(student);
             qStudents[qStudentI].checked = true;
-
             //if exists AND is the same
         } else if (qStudentI > -1 && Object.is(student, qStudents[qStudentI])) {
             qStudents[qStudentI].checked = true;
-
             //if doesn't exist in qualtrics
         } else {
             toAdd.push(student);
-            qStudents[qStudentI].checked = true;
         }
     });
 
     console.log("toAdd");
-    console.log(toAdd);
-//    console.log("\ntoUpdate");
-//    console.log(toUpdate);
+    console.log(toAdd.length);
+    console.log("\ntoUpdate");
+    console.log(toUpdate.length);
 
     console.log("\nqstudents");
     console.log(qStudents);
 
     // exists in qualtrics but not in file to be synked
-    var toTerminate = qStudents.filter(function () {
-        return qStudents.checked;
-        deleteStudents(toTerminate);
+    var toTerminate = qStudents.filter(function (qStudents) {
+        return !qStudents.checked;
     });
 
+    //    deleteStudents(toTerminate);
+    console.log("To Terminate");
+    console.log(toTerminate.length);
 
-
-
-    /*var tempStudentList = [],
-    tempStudent = {};
-for (var i = 0; i < students.length; i++) {
-    tempStudent = JSON.stringify({
-        firstName: students[i].PreferredName,
-        email: students[i].Email,
-        externalDataReference: students[i].UniqueID,
-        embeddedData: {
-            sessionOrder: students[i].SessionOrder,
-            username: students[i].Username,
-            semester: students[i].Semester,
-            course: students[i].Course,
-            section: students[i].Section,
-            subprogram: students[i].Subprogram,
-            department: students[i].Department,
-            pathway: students[i].Pathway,
-            block: students[i].Block,
-            gender: students[i].Gender,
-            countryOnline: students[i].CountryOnline,
-            state: students[i].State,
-            classification: students[i].Classification,
-            historicalAge: students[i].HistoricalAge,
-            major: students[i].Major
-        }
-    });
-    ss.addStudent(options[2], tempStudent);
-    tempStudentList.push(tempStudent);*/
-    //        console.log(JSON.stringify(tempStudent));
 }
 
 init();
