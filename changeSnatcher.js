720cafa5c0993a15f86cca7b73a2f79430091420
Danverde <gre12014@byui.edu>
2017-03-22 14:35:53 -0600

    WIP Now works, just need to send the modified links file to cli & handle any errors

--- changeSnatcher.js
+++ changeSnatcher.js
@@ -7,18 +7,58 @@ var cs = function () {},
 
 const fs = require('fs'),
     d3 = require('d3-dsv'),
-    hash = require('string-hash'),
+    createHash = require('string-hash'),
+    bs = require('binarysearch'),
     async = require('async');
 
 
 function saveHashes(links) {
+    var newHashes;
+    links.forEach(function (link) {
+        delete link.hash;
+    });
 
+    newHashes = d3.csvFormat(links);
+    console.log(newHashes);
+
+    fs.writeFile("hashes.csv", newHashes, function (err) {
+        if (err) throw err;
+    });
 }
 
 function compareHashes(links, hashes) {
+    var toUpdate;
+
     console.log("LINKS\n", links);
-    console.log("HASHES\n", hashes);
+    //console.log("HASHES\n", hashes);
+
+    toUpdate = links.filter(function (link) {
+        var hIndex;
+        // run a binary search
+        hIndex = bs(hashes, link, sortList);
+
+        //if found, compare
+        if (hIndex > -1) {
+            if (link.hash != hashes[hIndex].hash) {
+                console.log(link.hash);
+                console.log(hashes[hIndex].hash);
+
+                return link;
+            }
+        } else {
+            return link;
+        }
+    });
+
+    //update hashes.csv
+    saveHashes(links);
+
+    //start the sync!
+
+
+    console.log("TOUPDATE", toUpdate.length);
     console.log('FINISHED!');
+
 }
 
 
@@ -38,9 +78,11 @@ function getHashes(links) {
 }
 
 function hashLinks(link, cb) {
-    fs.readFile(link.csv, function (err, content) {
+    //ONLY FOR TESTING!
+    var file = 'lists/' + link.csv;
+    fs.readFile(file, function (err, content) {
         if (err) cb(err);
-        link.hash = hash(content.toString());
+        link.hash = createHash(content.toString());
         cb(null, link);
     });
 }
@@ -48,7 +90,6 @@ function hashLinks(link, cb) {
 var init = function (links) {
 
     async.map(links, hashLinks, function (err, links) {
-        console.log(links);
         getHashes(links);
     });
 
