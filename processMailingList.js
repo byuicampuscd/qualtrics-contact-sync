8884c79e66cb01686750b429fdb2c13daaf88adb
Danverde <gre12014@byui.edu>
2017-06-12 08:15:49 -0600

    re-ordered functions, more clear function names, created generateFileData function from async map

--- processMailingList.js
+++ processMailingList.js
@@ -16,7 +16,9 @@ var deepEqual = require('deep-equal'),
     ss = new StudentSnatcher(),
     os = new optionSnatcher();
 
-
+/**************************
+ * sort based on unique ID
+ **************************/
 function sortList(a, b) {
     if (a.externalDataReference < b.externalDataReference) return -1;
     if (a.externalDataReference > b.externalDataReference) return 1;
@@ -38,6 +40,91 @@ function sendFileError(err, cb) {
 
     lw.generateFile(result);
     cb(null, result);
+}
+
+/**********************************************************
+ * called once the API calls to qualtrics finish
+ * formats results of the calls, such as added, updated, 
+ * and deleted count
+ ********************************************************/
+function generateFileData(err, students) {
+    if (err) {
+        console.error(err);
+        sendFileError(err, cb);
+        return;
+    }
+    // file is returned to cli.js
+    var cb = this,
+        file = {
+            fileName: link.csv,
+            toAlterAmount: students.length,
+            aCount: 0,
+            uCount: 0,
+            dCount: 0,
+            passed: true,
+            studentErrors: [],
+            fileError: null
+        };
+    // sort through students and create report based on worked/error attributes
+    students.forEach(function (student) {
+        if (student.pass) {
+            if (student.action == "Add")
+                file.aCount++;
+            else if (student.action == "Update")
+                file.uCount++;
+            else
+                file.dCount++;
+        } else
+            file.studentErrors.push(student);
+    });
+
+    // generate report of completed additions/updates/deletions
+    if (file.aCount > 0)
+        console.log(chalk.green("Students successfully added: " + file.aCount));
+    if (file.uCount > 0)
+        console.log(chalk.green("Students successfully updated: " + file.uCount));
+    if (file.dCount > 0)
+        console.log(chalk.green("Students successfully deleted: " + file.dCount));
+
+    file.studentErrors.forEach(function (student) {
+        console.log(chalk.red("Failed to " +
+            student.action + " student: ") + student.externalDataReference, chalk.red("Error: " + student.errorMessage));
+    });
+    if (file.studentErrors.length)
+        file.passed = false;
+    // console.log('FILE:\n', file);
+
+    var wrapper = {
+        file: file,
+        link: link
+    };
+
+    lw.generateFile(wrapper);
+
+    // return to cli.js
+    cb(err, wrapper);
+
+}
+
+/***********************************************
+ * create approptriate API call for each action
+ ************************************************/
+function setOptions(student, callback) {
+    //    console.log(chalk.magenta('setOptions'));
+    var option = "";
+    // create approptriate API call
+    switch (student.action) {
+        case 'Add':
+            option = os.add(link.MailingListID, student);
+            break;
+        case 'Update':
+            option = os.update(link.MailingListID, student);
+            break;
+        case 'Delete':
+            option = os.delete(link.MailingListID, student.id);
+            break;
+    }
+    ss.send(student, option, callback);
 }
 
 /***********************************************
@@ -70,80 +157,11 @@ function filterStudent(student) {
     return filteredStudent;
 }
 
-/***********************************************
- * filter student object so the format matches 
- * qualtrics api format
- *************************************************/
-function formatStudents(students) {
-    // console.log(chalk.magenta('formatting Students'));
-    // create keys for embeddedData object
-    var emdKeys = Object.keys(students[0]).filter(function (key) {
-        return key != 'Email' && key != 'UniqueID' && key != 'FirstName';
-    });
-    // create keys for student object
-    var keys = Object.keys(students[0]).filter(function (key) {
-        return key == 'Email' || key == 'UniqueID' || key == 'FirstName';
-    });
-
-    var formattedStudents = students.map(function (currVal, formattedStudents) {
-        var tStudent = {},
-            tEmbeddedData = {};
-        // format keys and create tempStudent object
-        for (var j = 0; j < keys.length; j++) {
-            // UniqueID must be converted to externalDataReference
-            if (keys[j] === 'UniqueID') {
-                tStudent.externalDataReference = currVal[keys[j]];
-            } else {
-                // first letter of each key ot lower case
-                tStudent[keys[j][0].toLowerCase() + keys[j].slice(1)] = currVal[keys[j]];
-            }
-        }
-        var commaFinder = new RegExp(/,/g);
-
-        // create embeddedData object
-        for (var i = 0; i < emdKeys.length; i++) {
-            // filter commas out of embeddedData values so the qualtrics api won't throw a fit
-            tEmbeddedData[emdKeys[i]] = currVal[emdKeys[i]].replace(commaFinder, '');
-        }
-
-        if (emdKeys.length > 0)
-            tStudent.embeddedData = tEmbeddedData;
-
-        return tStudent;
-    });
-    return formattedStudents;
-}
-
-/***********************************************
- * create approptriate API call for each action
- ************************************************/
-function setOptions(student, callback) {
-    //    console.log(chalk.magenta('setOptions'));
-    var option = "";
-    // create approptriate API call
-    switch (student.action) {
-        case 'Add':
-            option = os.add(link.MailingListID, student);
-            break;
-        case 'Update':
-            option = os.update(link.MailingListID, student);
-            break;
-        case 'Delete':
-            option = os.delete(link.MailingListID, student.id);
-            break;
-    }
-    ss.send(student, option, callback);
-}
-
-
-function nameless(err, student) {
-
-}
-
-/*********************************************
- * basically does everything.... I need to break up this function
- **********************************************/
-function processTheData(students, cb, qStudents) {
+/********************************************************
+ * sorts the qualtrics list & csv lists and uses a binary
+ * search to check for equality
+ ********************************************************/
+function compareStudents(students, cb, qStudents) {
     console.log(chalk.magenta('Comparing Students'));
     var toAlter = [];
 
@@ -190,66 +208,15 @@ function processTheData(students, cb, qStudents) {
 
     console.log('Changes to be made: ', toAlter.length);
 
-    // make api calls X at a time - callback returns here
-    // async.mapLimit(toAlter, 10, setOptions, nameless.bind(cb));
-    async.mapLimit(toAlter, 10, setOptions, function (err, students) {
-        if (err) {
-            console.error(err);
-            sendFileError(err, cb);
-            return;
-        }
-        // file is returned to cli.js
-        var file = {
-            fileName: link.csv,
-            toAlterAmount: students.length,
-            aCount: 0,
-            uCount: 0,
-            dCount: 0,
-            passed: true,
-            studentErrors: [],
-            fileError: null
-        };
-        // sort through students and create report based on worked/error attributes
-        students.forEach(function (student) {
-            if (student.pass) {
-                if (student.action == "Add")
-                    file.aCount++;
-                else if (student.action == "Update")
-                    file.uCount++;
-                else
-                    file.dCount++;
-            } else
-                file.studentErrors.push(student);
-        });
-
-        // generate report of completed additions/updates/deletions
-        if (file.aCount > 0)
-            console.log(chalk.green("Students successfully added: " + file.aCount));
-        if (file.uCount > 0)
-            console.log(chalk.green("Students successfully updated: " + file.uCount));
-        if (file.dCount > 0)
-            console.log(chalk.green("Students successfully deleted: " + file.dCount));
-
-        file.studentErrors.forEach(function (student) {
-            console.log(chalk.red("Failed to " +
-                student.action + " student: ") + student.externalDataReference, chalk.red("Error: " + student.errorMessage));
-        });
-        if (file.studentErrors.length)
-            file.passed = false;
-        // console.log('FILE:\n', file);
-
-        var dataToSync = {
-            file: file,
-            link: link
-        };
-
-        lw.generateFile(dataToSync);
-
-        // return to cli.js
-        cb(err, dataToSync);
-    });
+    // make api calls X at a time -- callback returns here
+    // .bind sends the final cb to generateFileData as the this value
+    async.mapLimit(toAlter, 10, setOptions, generateFileData.bind(cb));
 }
 
+/*********************************************
+ * pulls student data from qualtrics until
+ * nextPage is false
+ **********************************************/
 function pullStudents(students, cb, qStudents, nextPage) {
     //    console.log(chalk.magenta('pullStudents'));
     if (!qStudents) qStudents = [];
@@ -265,23 +232,66 @@ function pullStudents(students, cb, qStudents, nextPage) {
             pullStudents(students, cb, qStudents, nextPage);
         } else {
             qStudents.sort(sortList);
-            processTheData(students, cb, qStudents);
+            compareStudents(students, cb, qStudents);
         }
     });
 }
-//cb returns to cli
-function init(dataToSync, cb) {
-    // console.log(chalk.magenta('init'));
-    // console.log(chalk.yellow('data to sync:\n'), dataToSync);
-    link = dataToSync.link;
 
-    if (link.matchingHashes == true) {
-        cb(null, dataToSync);
-        return;
-    }
+/***********************************************
+ * filter student object so the format matches 
+ * qualtrics api format
+ *************************************************/
+function formatStudents(students) {
+    // console.log(chalk.magenta('formatting Students'));
+    // create keys for embeddedData object
+    var emdKeys = Object.keys(students[0]).filter(function (key) {
+        return key != 'Email' && key != 'UniqueID' && key != 'FirstName';
+    });
+    // create keys for student object
+    var keys = Object.keys(students[0]).filter(function (key) {
+        return key == 'Email' || key == 'UniqueID' || key == 'FirstName';
+    });
+
+    var formattedStudents = students.map(function (currVal, formattedStudents) {
+        var tStudent = {},
+            tEmbeddedData = {};
+        // format keys and create tempStudent object
+        for (var j = 0; j < keys.length; j++) {
+            // UniqueID must be converted to externalDataReference
+            if (keys[j] === 'UniqueID') {
+                tStudent.externalDataReference = currVal[keys[j]];
+            } else {
+                // first letter of each key ot lower case
+                tStudent[keys[j][0].toLowerCase() + keys[j].slice(1)] = currVal[keys[j]];
+            }
+        }
+        var commaFinder = new RegExp(/,/g);
+
+        // create embeddedData object
+        for (var i = 0; i < emdKeys.length; i++) {
+            // filter commas out of embeddedData values so the qualtrics api won't throw a fit
+            tEmbeddedData[emdKeys[i]] = currVal[emdKeys[i]].replace(commaFinder, '');
+        }
+
+        if (emdKeys.length > 0)
+            tStudent.embeddedData = tEmbeddedData;
+
+        return tStudent;
+    });
+    return formattedStudents;
+}
+
+/******************************************
+ * Pull student data from csv & format it
+ * to match qualtrics API
+ *****************************************/
+function init(wrapper, cb) {
+    // console.log(chalk.yellow('data to sync:\n'), wrapper);
+    link = wrapper.link;
+
     console.log('\n', chalk.blue(link.csv));
     var filePath = 'Z:\\' + link.csv;
-    // get students from the tsv file
+    // get students from the csv file
     ss.readStudents(filePath, function (err, students) {
         if (err) {
             sendFileError(err, cb);
@@ -293,7 +303,7 @@ function init(dataToSync, cb) {
             return student.UniqueID && student.UniqueID !== '';
         });
 
-        // format csv student object for qualtrics
+        // format csv student object to match qualtrics API format
         if (students.length) {
             students = formatStudents(students);
             students.sort(sortList);
